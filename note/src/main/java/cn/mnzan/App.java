6921c7ead31abb791a3a1978b350a418e8615fb0
zxy <65475633+zxy-9426@users.noreply.github.com>
2024-01-16 10:31:55 +0800

    Add files via upload
    项目初始化

---
+++ note/src/main/java/cn/mnzan/App.java
@@ -0,0 +1,13 @@
+package cn.mnzan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
